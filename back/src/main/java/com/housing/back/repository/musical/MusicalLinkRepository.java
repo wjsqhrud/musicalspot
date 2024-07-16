520b78a18936db515f4640beba759c85cea91ab8
choihoseok <69013450+ghtjr410@users.noreply.github.com>
2024-07-16 10:44:59 +0900

    Delete back directory

--- back/src/main/java/com/housing/back/repository/musical/MusicalLinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.housing.back.repository.musical;
-
-import com.housing.back.entity.musical.MusicalLinkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MusicalLinkRepository extends JpaRepository<MusicalLinkEntity, Long> {
-    List<MusicalLinkEntity> findByMusicalId(Long musicalId);
-}
--- back/src/main/java/com/housing/back/repository/musical/MusicalTicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.housing.back.repository.musical;
-
-import com.housing.back.entity.musical.MusicalTicketEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MusicalTicketRepository extends JpaRepository<MusicalTicketEntity, Long> {
-    List<MusicalTicketEntity> findByMusicalId(Long musicalId);
-}
