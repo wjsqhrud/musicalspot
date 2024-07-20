2270884a97d5ef9613fcba5b44d540af35c3ed2d
ghtjr410 <ghtjr410@gmail.com>
2024-07-21 01:08:59 +0900

    0721 최호석 navigationFallback 설정

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -13,10 +13,12 @@ root.render(
     // <BrowserRouter>
     //   <App />
     // </BrowserRouter>
-
-    <Router>
-    <App />
-  </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  //   <Router>
+  //   <App />
+  // </Router>
   // </React.StrictMode>
 );
 // StrictMode를 키면 useeffect가 두번실행되서 껐습니다.
