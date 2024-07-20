ad959bf76a6d40a64b9b4dc6f4361df9f7c910e2
ghtjr410 <ghtjr410@gmail.com>
2024-07-21 01:17:20 +0900

    0721 최호석 hashrouter 실험

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css'; 
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,12 +13,9 @@ root.render(
     // <BrowserRouter>
     //   <App />
     // </BrowserRouter>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  //   <Router>
-  //   <App />
-  // </Router>
+    <Router>
+    <App />
+  </Router>
   // </React.StrictMode>
 );
 // StrictMode를 키면 useeffect가 두번실행되서 껐습니다.
