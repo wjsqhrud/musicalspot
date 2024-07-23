dca7b04300f64bb440b49d93f7ca11623802d6a5
wjsqhrud <anjella860@naver.com>
2024-07-23 09:42:12 +0900

    Reapply "0723 보경 상세페이지"
    This reverts commit bee065d39ff835483836eb08d8a575d431607bca.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,31 +1,36 @@
 // src/App.tsx// src/App.tsx
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { HeaderProvider } from 'services/HeaderService/HeaderService';
-import OAuth from 'services/OAuth2SignInService/oAuthResponseService';
-import SignInPage from 'apages/auth/SignInPage';
-import SignUpPage from 'apages/auth/SignUpPage';
-import CreateNickNamePage from 'apages/auth/CreateNickNamePage';
-import Mainpage from 'apages/main/Mainpage';
-import ReviewList from 'apages/reviewList/ReviewList';
-import TestPage from 'apages/test/TestPage';
-import MusicalPage from 'apages/musical/MusicalPage';
-import CategoryPage from 'apages/Category/CategoryPage';
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HeaderProvider } from "services/HeaderService/HeaderService";
+import OAuth from "services/OAuth2SignInService/oAuthResponseService";
+import SignInPage from "apages/auth/SignInPage";
+import SignUpPage from "apages/auth/SignUpPage";
+import CreateNickNamePage from "apages/auth/CreateNickNamePage";
+import Mainpage from "apages/main/Mainpage";
+import ReviewList from "apages/reviewList/ReviewList";
+import TestPage from "apages/test/TestPage";
+import MusicalPage from "apages/musical/MusicalPage";
+import CategoryPage from "apages/Category/CategoryPage";
+import DetailPage from "apages/detail/DetailPage";
 
 function App() {
   return (
     <HeaderProvider>
       <Routes>
-        <Route path='/auth'>
-          <Route path='home' element={<Mainpage/>} />
-          <Route path='category' element={<CategoryPage/>} />
-          <Route path='musical' element={<MusicalPage/>} />
-          <Route path='reviewlist' element={<ReviewList/>}/>
-          <Route path='sign-in' element={<SignInPage/>} />
-          <Route path='sign-up' element={<SignUpPage/>} />
-          <Route path='create-nickname' element={<CreateNickNamePage/>} />
-          <Route path='test' element={<TestPage/>} />
-          <Route path='oauth-response/:token/:expirationTime/:refreshToken/:refreshExpirationTime' element={<OAuth />} />
+        <Route path="/auth">
+          <Route path="details/:musicalId" element={<DetailPage />} />
+          <Route path="home" element={<Mainpage />} />
+          <Route path="category" element={<CategoryPage />} />
+          <Route path="musical" element={<MusicalPage />} />
+          <Route path="reviewlist" element={<ReviewList />} />
+          <Route path="sign-in" element={<SignInPage />} />
+          <Route path="sign-up" element={<SignUpPage />} />
+          <Route path="create-nickname" element={<CreateNickNamePage />} />
+          <Route path="test" element={<TestPage />} />
+          <Route
+            path="oauth-response/:token/:expirationTime/:refreshToken/:refreshExpirationTime"
+            element={<OAuth />}
+          />
         </Route>
         <Route path="*" element={<Navigate to="/auth/home" />} />
       </Routes>
--- front/src/services/musical/musicalService.ts
+++ front/src/services/musical/musicalService.ts
@@ -1,173 +1,200 @@
 import axios from "axios";
-import { ALL_MUSICALS, CATEGORY_LIST, CATEGORY_MUSICAL, MUSICAL_CATEGORYID_SORTED_BY_END_DATE, MUSICAL_CATEGORYID_SORTED_BY_START_DATE, MUSICAL_CATEGORYID_SORTED_BY_TITLE, MUSICAL_CATEGORYID_SORTED_BY_VIEW_COUNT, MUSICAL_DETAILS, MUSICAL_DETAILS_INCREMENT_VIEW, MUSICAL_LIKE, MUSICAL_SORTED_BY_START_DATE, MUSICAL_SORTED_BY_VIEW_COUNT, SEARCH_MUSICALS_BY_TITLE, TOGGLE_MUSICAL_LIKE } from "utils/APIUrlUtil/apiUrlUtil"
+import {
+  ALL_MUSICALS,
+  CATEGORY_LIST,
+  CATEGORY_MUSICAL,
+  MUSICAL_CATEGORYID_SORTED_BY_END_DATE,
+  MUSICAL_CATEGORYID_SORTED_BY_START_DATE,
+  MUSICAL_CATEGORYID_SORTED_BY_TITLE,
+  MUSICAL_CATEGORYID_SORTED_BY_VIEW_COUNT,
+  MUSICAL_DETAILS,
+  MUSICAL_DETAILS_INCREMENT_VIEW,
+  MUSICAL_LIKE,
+  MUSICAL_SORTED_BY_START_DATE,
+  MUSICAL_SORTED_BY_VIEW_COUNT,
+  SEARCH_MUSICALS_BY_TITLE,
+  TOGGLE_MUSICAL_LIKE,
+} from "utils/APIUrlUtil/apiUrlUtil";
 import { getCookie } from "utils/CookieUtil/cookieUtis";
 
-
-
-
 // 뮤지컬 카테고리 리스트 불러오기
-export const categoryList = async () =>{
-    try {
-        const result = await axios.get(CATEGORY_LIST());
-        return result.data;
-      } catch (error) {
-        console.error('Error ', error);
-        throw error;
-      }
-}
+export const categoryList = async () => {
+  try {
+    const result = await axios.get(CATEGORY_LIST());
+    return result.data;
+  } catch (error) {
+    console.error("Error ", error);
+    throw error;
+  }
+};
 
 // 제목으로 뮤지컬 검색 (검색창)
 export const searchMusicalsByTitle = async (title: string) => {
-    try {
-        const result = await axios.get(SEARCH_MUSICALS_BY_TITLE(title));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
+  try {
+    const result = await axios.get(SEARCH_MUSICALS_BY_TITLE(title));
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
 };
 
 // 선택 카테고리 뮤지컬 목록
-export const categoryMusical = async (categoryId : string) => {
-    try{
-        const result = await axios.get(CATEGORY_MUSICAL(categoryId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+export const categoryMusical = async (categoryId: string) => {
+  try {
+    const result = await axios.get(CATEGORY_MUSICAL(categoryId));
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 뮤지컬 최신 등록순 (메인) 7개
 export const musicalSortedByStartDate = async () => {
-    try{
-        const result = await axios.get(MUSICAL_SORTED_BY_START_DATE());
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(MUSICAL_SORTED_BY_START_DATE());
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 뮤지컬 조회수순 (메인) 10개
 export const musicalSortedByViewCount = async () => {
-    try{
-        const result = await axios.get(MUSICAL_SORTED_BY_VIEW_COUNT());
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(MUSICAL_SORTED_BY_VIEW_COUNT());
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // todo: All 눌렀을때 나오는 뮤지컬 전체목록나오는거 깜박하고 못했음
 // 모든 뮤지컬
 export const allMusicals = async () => {
-    try{
-        const result = await axios.get(ALL_MUSICALS());
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(ALL_MUSICALS());
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 해당 카테고리 뮤지컬 조회수순
-export const musicalCategoryIdSortedByViewCount = async (categoryId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_CATEGORYID_SORTED_BY_VIEW_COUNT(categoryId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+export const musicalCategoryIdSortedByViewCount = async (
+  categoryId: string
+) => {
+  try {
+    const result = await axios.get(
+      MUSICAL_CATEGORYID_SORTED_BY_VIEW_COUNT(categoryId)
+    );
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 해당 카테고리 뮤지컬 종료임박순
 export const musicalCategoryIdSortedByEndDate = async (categoryId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_CATEGORYID_SORTED_BY_END_DATE(categoryId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(
+      MUSICAL_CATEGORYID_SORTED_BY_END_DATE(categoryId)
+    );
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 해당 카테고리 뮤지컬 상품명순
 export const musicalCategoryIdSortedByTitle = async (categoryId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_CATEGORYID_SORTED_BY_TITLE(categoryId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(
+      MUSICAL_CATEGORYID_SORTED_BY_TITLE(categoryId)
+    );
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 해당 카테고리 뮤지컬 최신등록순
-export const musicalCategoryIdSortedByStartDate = async (categoryId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_CATEGORYID_SORTED_BY_START_DATE(categoryId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+export const musicalCategoryIdSortedByStartDate = async (
+  categoryId: string
+) => {
+  try {
+    const result = await axios.get(
+      MUSICAL_CATEGORYID_SORTED_BY_START_DATE(categoryId)
+    );
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 뮤지컬 상세보기 + 예매처 + 티켓가격
 export const musicalDetails = async (musicalId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_DETAILS(musicalId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(MUSICAL_DETAILS(musicalId));
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // 뮤지컬 상세보기 조회수 증가
 export const musicalDetailsIncrementView = async (musicalId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_DETAILS_INCREMENT_VIEW(musicalId));
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(MUSICAL_DETAILS_INCREMENT_VIEW(musicalId));
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
-
-// todo: 여기서부터하자 어우 졸려 
+// todo: 여기서부터하자 어우 졸려
 // ( 로그인사용자 ) 뮤지컬 상세보기 좋아요 누른적 있는지 (랜더링시)
 export const musicalLike = async (musicalId: string) => {
-    try{
-        const result = await axios.get(MUSICAL_LIKE(musicalId),{
-            headers: {
-                'Authorization': `Bearer ${getCookie('accessToken')}`
-            },
-            withCredentials: true
-        });
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.get(MUSICAL_LIKE(musicalId), {
+      headers: {
+        Authorization: `Bearer ${getCookie("accessToken")}`,
+      },
+      withCredentials: true,
+    });
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // ( 로그인사용자 ) 뮤지컬 상세보기 좋아요 클릭 (시점불분명)
 export const toggleMusicalLike = async (musicalId: string) => {
-    try{
-        const result = await axios.post(TOGGLE_MUSICAL_LIKE(musicalId), {},{
-            headers: {
-                'Authorization': `Bearer ${getCookie('accessToken')}`
-            },
-            withCredentials: true
-        });
-        return result.data;
-    } catch (error) {
-        console.error('Error', error);
-        throw error;
-    }
-} 
+  try {
+    const result = await axios.post(
+      TOGGLE_MUSICAL_LIKE(musicalId),
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${getCookie("accessToken")}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return result.data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
