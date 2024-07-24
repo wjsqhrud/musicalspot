4d5830490c1efd1ab39c1c90961b95d6b2e310f5
wjsqhrud <anjella860@naver.com>
2024-07-24 21:28:48 +0900

    0724 보경 바뀐 내용은 없는데 커밋하라고 함..

--- front/src/apages/main/Mainpage.tsx
+++ front/src/apages/main/Mainpage.tsx
@@ -54,10 +54,12 @@ const Mainpage: React.FC = () => {
   const formatDate = (dateString: string | undefined) => {
     if (!dateString) return "";
     const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    return `${year}.${month}.${day}`;
+    const formatter = new Intl.DateTimeFormat("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+    return formatter.format(date).replace(/\. /g, ".").replace(".", "");
   };
 
   // 조회수 기준으로 정렬된 뮤지컬 목록을 가져오는 함수
@@ -186,6 +188,9 @@ const Mainpage: React.FC = () => {
             ))}
           </div>
         </div>
+
+        {/* WHAT'S HOT 텍스트 */}
+        <div className="text-center text-2xl font-bold my-10">WHAT'S HOT</div>
 
         {/* Grid Section */}
         <div className="p-5 grid grid-cols-5 gap-4">
