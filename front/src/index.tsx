77b15fef6bc5bd7ddcfc974b945d0a19389ef448
ghtjr410 <ghtjr410@gmail.com>
2024-07-29 20:15:36 +0900

    0729 최호석 spa문제 해결 시도

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter as Router } from 'react-router-dom';
+// import { BrowserRouter, HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'; 
 // import { BrowserRouter } from 'react-router-dom';
@@ -10,9 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
+    // <BrowserRouter>
       <App />
-    </BrowserRouter>
+    // </BrowserRouter>
   //   <Router>
   //   <App />
   // </Router>
