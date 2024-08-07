c34f6682f0ab5bed13dc92e317418dc3cae3e4b9
wjsqhrud <anjella860@naver.com>
2024-08-07 11:16:45 +0900

    0807 최호석 배포시도

--- back/src/main/java/com/housing/back/config/WebSocketConfig.java
+++ back/src/main/java/com/housing/back/config/WebSocketConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
+//ddsa
 import com.housing.back.handler.WebSocketHandshakeInterceptor;
 
 @Configuration
